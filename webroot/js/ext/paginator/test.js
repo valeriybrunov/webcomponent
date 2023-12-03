6c7d173da09b65456ea3dbe5840dbc165358b24f
valeriy-brunov <valer1978@bk.ru>
2023-12-03 15:58:00 +0400

    Тесты для paginator.

--- webroot/js/ext/paginator/test.js
+++ webroot/js/ext/paginator/test.js
@@ -55,6 +55,57 @@ describe("Тест расширяемого вэб-компонента Paginato
         });
 
     });
+
+    describe(`Проверяем метод clearElements():`, function() {
+
+    	beforeEach(() => {
+    		paginator = new Paginator();
+    	});
+
+        afterEach(() => {
+        	paginator = null;
+        });
+
+        it(`Скрытое поле присутствует.`, function() {
+        	paginator.insertAdjacentHTML('beforeend', `
+        		<div class="paste__replace">
+        			<input type="hidden" name="page" value="">
+        		</div>
+        		<div class="paste__click paginator__click button">
+    				<div class="text">Смотреть ещё...</div>
+				</div>
+				<div class="paste__trubber">мой труббер</div>
+        	`);
+        	paginator.clearElements();
+        	let input = paginator.querySelector('input[name="page"]');
+        	let replace = paginator.querySelector('.paste__replace');
+            let click = paginator.querySelector('.paste__click');
+            let trubber = paginator.querySelector('.paste__trubber');
+            assert.equal(input != null, true, `Скрытое поле отсутствует!`);
+        	assert.equal(replace != null, true, `Элемент с классом replace отсутствует!`);
+        	assert.equal(click != null, true, `Элемент с классом click отсутствует!`);
+        	assert.equal(trubber != null, true, `Элемент с классом trubber отсутствует!`);
+        });
+
+        it(`Скрытое поле отсутствует.`, function() {
+        	paginator.insertAdjacentHTML('beforeend', `
+        		<div class="paste__replace"></div>
+        		<div class="paste__click paginator__click button">
+    				<div class="text">Смотреть ещё...</div>
+				</div>
+				<div class="paste__trubber">мой труббер</div>
+        	`);
+        	paginator.clearElements();
+        	let input = paginator.querySelector('input[name="page"]');
+        	let replace = paginator.querySelector('.paste__replace');
+            let click = paginator.querySelector('.paste__click');
+            let trubber = paginator.querySelector('.paste__trubber');
+            assert.equal(input == null, true, `Скрытое поле присутствует!`);
+        	assert.equal(replace == null, true, `Элемент с классом replace присутствует!`);
+        	assert.equal(click == null, true, `Элемент с классом click присутствует!`);
+        	assert.equal(trubber == null, true, `Элемент с классом trubber присутствует!`);
+        });
+    });
 });
 
 /**
