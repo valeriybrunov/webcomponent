e5cfe7330a7a8db3d7532a6a1dec6c0c19b8ce4a
valeriy-brunov <valer1978@bk.ru>
2023-12-07 18:34:48 +0400

    Дополнил код.

--- webroot/js/ext/progress/progress.js
+++ webroot/js/ext/progress/progress.js
@@ -17,19 +17,51 @@ export default class Progress extends Paste {
         super();
         this.classList.add('progress');
         this.insertAdjacentHTML( 'afterbegin', Template.render() );
-        this.currentValue = 0;// Текущее значение прогресс-бара для отображения.
-        this.valueSlow = 0;// Значение "медленного" прогресс-бара.
-        this.valueFast = 0;// Значение "быстрого" прогресс-бара.
-        this.limit = 0;//
+        this.init = 0;// Указывает на первоначальную загрузку.
     }
 
     /**
      * Браузер вызывает этот метод при добавлении элемента в документ.
      * (может вызываться много раз, если элемент многократно добавляется/удаляется).
      */
-    connectedCallback() {
-        
-        super.connectedCallback();
+    //connectedCallback() {
+        //super.connectedCallback();
+    //}
+
+    /**
+     * Следим за изменениями этих атрибутов и отвечаем соответственно.
+     *
+     * @param string name Имя атрибута, в котором произошли изменения.
+     * @param string oldVal Старое значение атрибута, т.е. до его изменения.
+     * @param string newVal Новое значение атрибута.
+     * @return void
+     *
+     * !!! При первой загрузке страницы, если атрибуты установлены в веб-компоненте, происходит
+     *     срабатывание данной функции, при этом "oldVal=null", а "newVal" будет равно значению,
+     *     установленному в веб-компоненте.
+     */
+    attributeChangedCallback(name, oldVal, newVal) {
+        if ( oldVal ) {
+            switch ( name ) {// При обновление значений.
+                case 'url':// Для атрибута 'url'.
+                    this.init = 1;// Указывает на изменения атрибута "url".
+                    break;
+            }
+        }
+        super.attributeChangedCallback(name, oldVal, newVal);
+    }
+
+    /**
+     * Сброс к первоначальным установкам.
+     *
+     * @return void
+     */
+    reset() {
+        this.currentValue = 0;// Текущее значение прогресс-бара для отображения.
+        this.valueSlow = 0;// Значение "медленного" прогресс-бара.
+        this.valueFast = 0;// Значение "быстрого" прогресс-бара.
+        this.limit = 0;
+        this.dom.tagProgressBar.setAttribute('style', `width:0%`);
     }
 
     /**
@@ -40,6 +72,7 @@ export default class Progress extends Paste {
     moveProgressBar() {
         this.currentValue = Math.max(this.valueSlow, this.valueFast);
         this.dom.tagProgressBar.setAttribute('style', `width:${this.currentValue}%`);
+        if (this.currentValue == 100) this.reset();
     }
 
     /**
@@ -68,5 +101,50 @@ export default class Progress extends Paste {
         }
         this.moveProgress();
         if (this.valueFast < 100 && this.currentValue < 100) setTimeout(this.fast, 5);
+    }
+
+    /**
+     * AJAX-запрос.
+     *
+     * @return void
+     */
+    query() {
+        if (this.checkUrl() == 1 || this.checkUrl() == 3) return;
+        let self = this;
+        Ajax.connect({
+            url: self.url,
+            success: function(html) {
+                if (self.init == 0) self.initDownload();
+                if (self.init == 1) self.nextDownload();
+            },
+            error: function(status, statusText) {},
+            errorConnect: function() {},
+        });
+    }
+
+    /**
+     * При первоначальной загрузке страницы.
+     *
+     * @return void
+     */
+    initDownload() {
+        this.success(
+            this.timer(
+                this.resetTimerId(
+                    this.checkMode(
+                        this.replaceHTML(
+                            this.moveTagsScript(html)
+                        )
+                    )
+                )
+            )
+        );
+    }
+
+    /**
+     *
+     */
+    nextDownload() {
+
     }
 }
